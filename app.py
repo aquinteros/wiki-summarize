64013a800e3529c69dc73b4b0ccb7bfc136afd9e
aquinteros <alvaro.quinteros.a@gmail.com>
2023-06-07 20:41:42 -0400

    production

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, request
-import py_interface
+from resources.functions import *
 
-app = Flask(__name__)
+def run():
 
-@app.route('/wtn', methods=['GET'])
-def wtn():
-    concept = request.args.get('concept')
-    language = request.args.get('language')
-    output = py_interface.main(concept, language)
-    return output
+    # headers = {
+    # "Authorization": "Bearer " + notion_token,
+    # "Content-Type": "application/json",
+    # "Notion-Version": "2022-06-28"
+    # }
+    
+    headers = {
+        "Authorization": st.secrets["api_key"],
+        "Content-Type": "application/json",
+    }
 
-if __name__ == '__main__':
-    app.run()
+    st.title("Wiki Summary")
+
+    st.text("Importa un resumen de una página de Wikipedia a Notion")
+
+    page_name = st.text_input("Nombre de la página")
+
+    language = st.selectbox("Idioma", ['en', 'es'])
+
+    if st.button("Importar"):
+
+        output = return_summary(page_name, language)
+        
+        st.download_button('Descarga MD', output, page_name + ".md", "Descargar")
+
+        st.markdown(output)
+
+if __name__ == "__main__":
+    run()
